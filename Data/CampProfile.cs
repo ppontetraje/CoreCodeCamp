63aaa251447b47460e02f7592cd08497401ce145
Jean Piert Palomino Quispe <dragon3706@hotmail.com>
2021-03-11 19:22:51 -0500

    Agregar archivos de proyecto.

---
+++ Data/CampProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Data
+{
+    public class CampProfile : Profile
+    {
+        public CampProfile()
+        {
+            this.CreateMap<Camp, CampModel>()
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName));
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
+namespace CoreCodeCamp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+
+                app.UseRouting();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.UseEndpoints(cfg =>
+                {
+                    cfg.MapControllers();
+                });
+        }
+    }
+}
